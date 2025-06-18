0f52810bca267b496d1eaca6325a4d11ee564240
mustafamahmou23 <mustafamahmou23@gmail.com>
2025-06-18 20:26:35 +0300

    first

--- js/script.js
+++ js/script.js
@@ -21,3 +21,57 @@ allitems.forEach(function (item){
 btn.onclick = function () {
     console.log(totalprice);
 }
+
+
+// let markup = `
+//     <div>
+//         <h1>total price</h1>
+//         <button>buy</button>
+//         <button>cancel</button>
+//     </div>
+// `;
+// document.write(markup);
+
+// function loadData() {
+//     var xhttp = new XMLHttpRequest();
+
+//     xhttp.onreadystatechange = function () {
+//     console.log(JSON.parse(this.responseText));
+
+//         if (this.readyState == 4 && this.status == 200) {
+//             var data = JSON.parse(this.responseText);
+//         }
+//     };
+//     xhttp.open("GET", "https://jsonplaceholder.typicode.com/posts");
+//     xhttp.send();
+// }
+// loadData();
+
+
+
+//use Object
+// var car = {
+//     name: "bmw",
+//     price: 100000,
+//     color: "red"
+// }
+
+// console.log(car);
+
+// var book = new Object();
+// book.name = "javascript";
+// book.price = 100;
+// book.author = "ali";
+
+// console.log(book);
+
+document.title = "car.name";
+
+
+var heading = document.getElementById("demo");
+heading.textContent = "tjgjgiuyri";
+heading.style.fontSize = "80px";
+heading.style.color = "red";
+
+var span1 = document.getElementsByClassName("span1");
+span1.style.opacity = 1;
